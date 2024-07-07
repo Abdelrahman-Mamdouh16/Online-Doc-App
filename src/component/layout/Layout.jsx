30756695359a195dedc43af0410da95753dcd347
Abdelrahman-Mamdouh16 <bedo.lela32@gmail.com>
2024-07-07 20:09:30 +0300

    layout Update

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -1,29 +1,26 @@
 import React, { useEffect } from 'react'
 import Navbar from '../Navbar/Navbar.jsx'
 import Footer from '../Footer/Footer.jsx'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom'
 import { Offline } from "react-detect-offline";
 import './layout.css'
-import { useDispatch } from 'react-redux';
-// import { setLoading } from '../../Redux/Slices/userLoginData.slice.js';
+
 export default function Layout() {
     const location = useLocation()
     const pathName = location.pathname;
-    // const navigation = useNavigate()
-    const dispatch = useDispatch()
+
 
     useEffect(() => {
         if (pathName !== '/Reservation/create') {
             localStorage.removeItem('firstLoadDone')
         }
-        // if (pathName!=='/Account/userDetails'||pathName!=='') {
-        //     dispatch(setLoading(false));
-        // }
+        
     }, [pathName])
     return (
         <>
             <div className='app bgColorLite'>
                 <Navbar />
+                <ScrollRestoration/> 
                 <Outlet />
                 <div className='container w-50 position-fixed fixed-bottom ' style={{ bottom: '10%', zIndex: '1100' }}>
                     <Offline>
